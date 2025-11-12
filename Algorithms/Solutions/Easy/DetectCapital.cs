aaee21728417f6c0a0535ace7a1935d767685eae
rdvnabay <ridvanabay@gmail.com>
2025-11-12 17:07:00 +0300

    Solution 520: Detect Capital

---
+++ Algorithms/Solutions/Easy/DetectCapital.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithms.Solutions.Easy;
+
+public static class DetectCapital
+{
+    public static bool Run(string word)
+    {
+        if (word.Length == 1)
+            return true;
+
+        bool firstIsUpper = char.IsUpper(word[0]);
+        bool secondIsUpper = char.IsUpper(word[1]);
+
+        if (!firstIsUpper && secondIsUpper)
+            return false;
+
+        bool otherIsUpper = false;
+
+        if (firstIsUpper && secondIsUpper)
+            otherIsUpper = true;
+
+        for (int i = 2; i < word.Length; i++)
+        {
+            if (otherIsUpper)
+            {
+                if (char.IsLower(word[i]))
+                    return false;
+            }
+            else
+            {
+                if (char.IsUpper(word[i]))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
