e32b210ae3ffd7e005a2d959660f0b6ec0391312
rdvnabay <ridvanabay@gmail.com>
2025-11-12 15:15:35 +0300

    Solution 485: Max Consecutive Ones

---
+++ Algorithms/Solutions/Easy/MaxConsecutiveOnes.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Solutions.Easy;
+
+public static class MaxConsecutiveOnes
+{
+    public static int Run(int[] nums)
+    {
+        int max = 0;
+        int counter = 0;
+        int lenght = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                counter++;
+            }
+            else
+            {
+                if (counter > max)
+                {
+                    max = counter;
+                }
+                counter = 0;
+            }
+        }
+
+        if (counter > max)
+            return counter;
+
+        return max;
+    }
+}
