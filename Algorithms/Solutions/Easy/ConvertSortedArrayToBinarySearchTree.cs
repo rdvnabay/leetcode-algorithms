209db24b802c58022b9ad6f57fc7700f94409157
rdvnabay <ridvanabay@gmail.com>
2026-07-29 12:10:30 +0300

    Add unaccepted LeetCode solution attempts

---
+++ Algorithms/Solutions/Easy/ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿namespace Algorithms.Solutions.Easy;
+
+public class ConvertSortedArrayToBinarySearchTree
+{
+    public static TreeNode Run(int[] nums)
+    {
+        var isEven = nums.Length % 2 == 0;
+
+        return isEven
+            ? EvenTree(nums)
+            : OddTree(nums);
+    }
+
+    public static TreeNode OddTree(int[] nums)
+    {
+        var midIndex = nums.Length / 2;
+        var rootValue = nums[midIndex];
+
+        TreeNode tree = new(rootValue);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i == midIndex)
+                continue;
+
+            if (nums[i] < rootValue)
+            {
+                if (tree.left is null)
+                    tree.left = new TreeNode(nums[i]);
+                else
+                {
+                    var currentLeft = tree.left;
+                    currentLeft.left = new TreeNode(nums[i]);
+                }
+            }
+            else
+            {
+                if (tree.right is null)
+                    tree.right = new TreeNode(nums[i]);
+                else
+                {
+                    var currentRight = tree.right;
+                    currentRight.right = new TreeNode(nums[i]);
+                }
+            }
+        }
+
+        return tree;
+    }
+
+    public static TreeNode EvenTree(int[] nums)
+    {
+        return null;
+    }
+}
+
+//Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
--- Algorithms/Solutions/Medium/3Sum.cs
+++ Algorithms/Solutions/Medium/3Sum.cs
@@ -2,7 +2,7 @@
 
 public static class _3Sum
 {
-    public static IList<IList<int>> Run(int[] nums)
+    public static IList<IList<int>> Run2(int[] nums)
     {
         List<List<int>> result = new();
         List<string> concatenateNumbers = new();
@@ -20,8 +20,44 @@ public static class _3Sum
 
                         if (!result.Any(x => x[0] == output[0] && x[1] == output[1] && x[2] == output[2]))
                             result.Add(output);
-                    }
 
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+
+
+    public static IList<IList<int>> Run(int[] nums)
+    {
+        List<List<int>> result = new();
+        List<string> concatenateNumbers = new();
+
+        int firstNumberIndex = 0;
+        int firstNumber = nums[firstNumberIndex];
+
+        for (int j = firstNumberIndex + 1; j < nums.Length - 1; j++)
+        {
+            for (int k = j + 1; k < nums.Length; k++)
+            {
+                if (j == nums.Length - 1)
+                {
+                    firstNumberIndex++;
+                    firstNumber = nums[firstNumberIndex];
+                }
+
+                if (nums[firstNumberIndex] + nums[j] + nums[k] == 0)
+                {
+                    var output = new List<int> { nums[firstNumberIndex], nums[j], nums[k] };
+                    output.Sort();
+
+                    if (!result.Any(x => x[0] == output[0] && x[1] == output[1] && x[2] == output[2]))
+                        result.Add(output);
+
+                    break;
                 }
             }
         }
