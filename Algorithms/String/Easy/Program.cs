ea985dea940655df4bdb8a37e1829a1511ecc352
rdvnabay <ridvanabay@gmail.com>
2026-07-30 14:25:40 +0300

    242: Valid Anagram

--- Algorithms/String/Easy/Program.cs
+++ Algorithms/String/Easy/Program.cs
@@ -22,5 +22,6 @@ public static class StringEasyRunner
         //FindTheIndexOfTheFirstOccurrenceInAString.Run("leetcode", "leeto");
         FindTheIndexOfTheFirstOccurrenceInAString.Run("mississippi", "issip");
         ExcelSheetColumnTitle.Run(701);
+        ValidAnagram.Run("listen", "silent");
     }
 }
