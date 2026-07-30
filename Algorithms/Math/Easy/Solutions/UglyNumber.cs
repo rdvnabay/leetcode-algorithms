a89d24f480df33f4f60f75a99fae95636c83f6fb
rdvnabay <ridvanabay@gmail.com>
2026-07-30 16:37:42 +0300

    263 - Ugly Number

---
+++ Algorithms/Math/Easy/Solutions/UglyNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.Math.Easy.Solutions;
+
+public static class UglyNumber
+{
+    public static bool Run(int n)
+    {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+
+        int counter = 2;
+        return Recursive(n, counter);
+    }
+
+    static bool Recursive(int n, int counter)
+    {
+        if (n == 1)
+            return true;
+
+        if (counter > 5)
+            return false;
+
+        if (n % counter == 0)
+            n = n / counter;
+        else
+            counter++;
+
+        return Recursive(n, counter);
+    }
+}
