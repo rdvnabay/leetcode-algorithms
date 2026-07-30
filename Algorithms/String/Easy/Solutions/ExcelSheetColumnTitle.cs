8ba0f02ca6cca502b302abcb02e9462cc5bdbc9f
rdvnabay <ridvanabay@gmail.com>
2026-07-30 12:36:08 +0300

    Solve LeetCode 168 - Excel Sheet Column Title

---
+++ Algorithms/String/Easy/Solutions/ExcelSheetColumnTitle.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Algorithms.String.Easy.Solutions;
+
+public static class ExcelSheetColumnTitle
+{
+    public static string Run(int columnNumber)
+    {
+        StringBuilder sb = new();
+        Recursive(columnNumber, sb);
+
+        return sb.ToString();
+    }
+
+    static void Recursive(int dividend, StringBuilder sb)
+    {
+        int divisor = 26;
+        dividend--;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        sb.Insert(0, (char)('A' + remainder));
+
+        if (quotient == 0)
+            return;
+
+        Recursive(quotient, sb);
+    }
+}
