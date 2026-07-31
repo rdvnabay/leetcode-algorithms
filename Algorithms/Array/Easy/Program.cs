8bdf80eceac25437c7e4ff89db26992b3c8eb430
rdvnabay <ridvanabay@gmail.com>
2026-07-31 11:49:33 +0300

    867 - Transpose Matrix

--- Algorithms/Array/Easy/Program.cs
+++ Algorithms/Array/Easy/Program.cs
@@ -39,6 +39,7 @@ public static class ArrayEasyRunner
         MostCommonWord.Run("Bob hit a ball, the hit BALL flew far after it was hit.", new string[] { "hit" });
         ReshapeTheMatrix.Run(new int[][] { [1, 2], [3, 4] }, 1, 4);
         RangeAdditionII.Run(3, 3, new int[][] { [2, 2], [3, 3] });
-        LemonadeChange.Run(new int[]{ 5, 5, 10, 5, 20, 5, 5, 5, 5, 5, 20, 5, 10, 5, 5, 5, 5, 20, 20, 5 });
+        LemonadeChange.Run(new int[] { 5, 5, 10, 5, 20, 5, 5, 5, 5, 5, 20, 5, 10, 5, 5, 5, 5, 20, 20, 5 });
+        TransposeMatrix.Run(new int[][] { [1, 2, 3], [4, 5, 6] });
     }
 }
