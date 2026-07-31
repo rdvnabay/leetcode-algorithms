8b04db8de3991b4451421abd1bba7744a36bbb5c
rdvnabay <ridvanabay@gmail.com>
2026-07-31 19:10:00 +0300

    387 - First Unique Character in a String

---
+++ Algorithms/HashTable/Easy/Solutions/FirstUniqueCharacterInAString.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithms.HashTable.Easy.Solutions;
+
+public static class FirstUniqueCharacterInAString
+{
+    public static int Run(string s)
+    {
+        Dictionary<char, (int Index, int Count)> dict = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dict.TryGetValue(s[i], out var value))
+                dict.Add(s[i], (i, 1));
+            else
+                dict[s[i]] = (value.Index, value.Count += 1);
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value.Count == 1)
+                return item.Value.Index;
+        }
+
+        return -1;
+    }
+}
