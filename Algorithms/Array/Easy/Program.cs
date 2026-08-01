731b59c9a6cb00d649300c4dc581a29bd26769e1
rdvnabay <ridvanabay@gmail.com>
2026-08-01 14:54:55 +0300

    977 - Squares of a Sorted Array

--- Algorithms/Array/Easy/Program.cs
+++ Algorithms/Array/Easy/Program.cs
@@ -47,5 +47,6 @@ public static class ArrayEasyRunner
         ValidMountainArray.Run(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
         DeleteColumnsToMakeSorted.Run(new string[] { "cba", "daf", "ghi" });
         NRepeatedElementInSize2NArray.Run(new int[] { 1, 2, 3, 3 });
+        SquaresOfASortedArray.Run(new int[] { -4, -2, 0, 3, 5 });
     }
 }
---
+++ Algorithms/Array/Easy/Solutions/SquaresOfASortedArray.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+
+namespace Algorithms.Array.Easy.Solutions;
+
+public static class SquaresOfASortedArray
+{
+    public static int[] Run(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] *= nums[i];
+        }
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int current = nums[i];
+                int next = nums[j];
+
+                if (current > next)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
