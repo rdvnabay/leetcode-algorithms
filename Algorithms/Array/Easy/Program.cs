ff64ec5c1474264e74a7cc008169f1b4929135e8
rdvnabay <ridvanabay@gmail.com>
2026-08-01 12:59:35 +0300

    944 - Delete Columns to Make Sorted

--- Algorithms/Array/Easy/Program.cs
+++ Algorithms/Array/Easy/Program.cs
@@ -45,5 +45,6 @@ public static class ArrayEasyRunner
         SortArrayByParity.Run(new int[] { 3, 1, 2, 4 });
         _1BitAnd2BitCharacters.Run(new int[] { 1, 1, 1, 0 });
         ValidMountainArray.Run(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        DeleteColumnsToMakeSorted.Run(new string[] { "cba", "daf", "ghi" });
     }
 }
---
+++ Algorithms/Array/Easy/Solutions/DeleteColumnsToMakeSorted.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms.Array.Easy.Solutions;
+
+public static class DeleteColumnsToMakeSorted
+{
+    public static int Run(string[] strs)
+    {
+        int rows = strs.Length;
+        int cols = strs[0].Length;
+        int result = 0;
+
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows - 1; j++)
+            {
+                if (strs[j][i] > strs[j + 1][i])
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
