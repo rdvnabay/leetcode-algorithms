aaa3608d11b401740342322e73cc5a206a048fcc
rdvnabay <ridvanabay@gmail.com>
2026-08-02 07:03:40 +0300

    832 - Flipping an Image

---
+++ Algorithms/Array/Easy/FlippingAnImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Easy;
+
+public static class FlippingAnImage
+{
+    public static int[][] Run(int[][] image)
+    {
+        int rows = image.Length;
+        int cols = image[0].Length;
+
+        for (int i = 0; i < rows; i++)
+        {
+            int left = 0;
+            int right = cols - 1;
+
+            while (left <= right)
+            {
+                if (left != right)
+                {
+                    int temp = image[i][left];
+                    image[i][left] = image[i][right];
+                    image[i][right] = temp;
+
+                    image[i][left] = 1 - image[i][left];
+                    image[i][right] = 1 - image[i][right];
+                }
+
+                else
+                    image[i][left] = 1 - image[i][left];
+
+                left++;
+                right--;
+            }
+        }
+
+        return image;
+    }
+}
--- Algorithms/Array/Easy/Program.cs
+++ Algorithms/Array/Easy/Program.cs
@@ -52,5 +52,6 @@ public static class ArrayEasyRunner
         AddToArrayFormOfInteger.Run(new int[] { 1, 2, 0, 0 }, 34);
         MaximizeSumOfArrayAfterKNegations.Run(new int[] { 5, 6, 9, -3, 3 }, 2);
         HeightChecker.Run(new int[] { 1, 1, 4, 2, 1, 3 });
+        FlippingAnImage.Run(new int[][] { [1, 1, 0], [1, 0, 1], [0, 0, 0] });
     }
 }
