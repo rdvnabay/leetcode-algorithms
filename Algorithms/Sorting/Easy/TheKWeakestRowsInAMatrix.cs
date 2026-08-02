8b74e62ac2f7b2034c4529446f2199cee1351fd5
rdvnabay <ridvanabay@gmail.com>
2026-08-02 17:15:48 +0300

    1337 - The K Weakest Rows in a Matrix

---
+++ Algorithms/Sorting/Easy/TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorting.Easy;
+
+public static class TheKWeakestRowsInAMatrix
+{
+    public static int[] Run(int[][] mat, int k)
+    {
+        int rows = mat.Length;
+        int cols = mat[0].Length;
+        Dictionary<int, int> dict = new();
+
+        for (int i = 0; i < rows; i++)
+        {
+            int counter = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                if (mat[i][j] == 1)
+                    counter++;
+                else
+                    break;
+            }
+            dict.Add(i, counter);
+        }
+
+
+        return dict.OrderBy(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+    }
+}
---
+++ Algorithms/Sorting/Program.cs
@@ -0,0 +1,10 @@
+﻿using Sorting.Easy;
+
+TheKWeakestRowsInAMatrix.Run(new int[][]
+{
+    new int[] { 1, 1, 0, 0, 0 },
+    new int[] { 1, 1, 1, 1, 0 },
+    new int[] { 1, 0, 0, 0, 0 },
+    new int[] { 1, 1, 0, 0, 0 },
+    new int[] { 1, 1, 1, 1, 1 }
+}, k: 3);
