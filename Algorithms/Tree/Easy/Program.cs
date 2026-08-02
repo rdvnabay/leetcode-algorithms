bb38a171c31b11fe237ca53eaca6f9630a0f34a4
rdvnabay <ridvanabay@gmail.com>
2026-08-02 19:24:46 +0300

    100 - Same Tree

--- Algorithms/Tree/Easy/Program.cs
+++ Algorithms/Tree/Easy/Program.cs
@@ -1,4 +1,5 @@
 using Algorithms.Common;
+using Tree.Easy;
 
 namespace Algorithms.Tree.Easy;
 
@@ -7,6 +8,7 @@ public static class TreeEasyRunner
     public static void Run()
     {
         ConvertSortedArrayToBinarySearchTree.Run(new int[] { -10, -3, 0, 5, 9 });
+        SameTree.Run(new TreeNode(1, left: new TreeNode(2)), new TreeNode(1, right: new TreeNode(2)));
         //BinaryTreePaths.Run(
         //    new TreeNode(1, left: new TreeNode(2, right: new TreeNode(5)), right: new TreeNode(3)));
     }
---
+++ Algorithms/Tree/Easy/SameTree.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Common;
+
+namespace Tree.Easy;
+
+public static class SameTree
+{
+    public static bool Run(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        bool isLeftNodeEqual = Run(p.left, q.left);
+        bool isRightNodeEqual = Run(p.right, q.right);
+
+        return isLeftNodeEqual && isRightNodeEqual;
+    }
+}
