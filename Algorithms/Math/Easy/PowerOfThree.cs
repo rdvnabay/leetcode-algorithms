aba616d4a45e221c538ea75e09ae0a2ccb58554d
rdvnabay <ridvanabay@gmail.com>
2026-08-03 12:37:42 +0300

    326 - Power of Three

---
+++ Algorithms/Math/Easy/PowerOfThree.cs
@@ -0,0 +1,22 @@
+﻿namespace Math.Easy;
+
+public static class PowerOfThree
+{
+    public static bool Run(int n)
+    {
+        const int X = 3;
+
+        if (n <= 0) return false;
+        if (n == 1) return true;
+
+        while (n > 1)
+        {
+            if (n % X != 0)
+                return false;
+
+            n = n / X;
+        }
+        
+        return true;
+    }
+}
--- Algorithms/Math/Program.cs
+++ Algorithms/Math/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("=== Math Algorithms ===");
 MathEasyRunner.Run();
 MathMediumRunner.Run();
 AddBinary.Run("11", "1");
+PowerOfThree.Run(27);
