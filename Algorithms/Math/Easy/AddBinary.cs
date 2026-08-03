eb35b06d4dd2b94826528973bb2699c815dceb96
rdvnabay <ridvanabay@gmail.com>
2026-08-03 11:58:23 +0300

    67 - Add Binary

---
+++ Algorithms/Math/Easy/AddBinary.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Math.Easy;
+
+public static class AddBinary
+{
+    public static string Run(string a, string b)
+    {
+        int maxLength = System.Math.Max(a.Length, b.Length);
+
+        a = a.PadLeft(maxLength, '0');
+        b = b.PadLeft(maxLength, '0');
+
+        int carry = 0;
+        StringBuilder sb = new();
+
+        for (int i = maxLength - 1; i >= 0; i--)
+        {
+            var result = System.Math.Abs(a[i] - b[i]);
+
+            if (result == 0)
+            {
+                if (a[i] == '1' || b[i] == '1')
+                {
+                    sb.Append((result + carry) % 2);
+                    carry = 1;
+                }
+                else
+                {
+                    sb.Append((result + carry) % 2);
+                    carry = 0;
+                }
+            }
+            else
+            {
+                sb.Append((result + carry) % 2);
+            }
+        }
+
+        if (carry == 1)
+            sb.Append(carry);
+
+        return new string(sb.ToString().Reverse().ToArray());
+    }
+}
--- Algorithms/Math/Program.cs
+++ Algorithms/Math/Program.cs
@@ -1,6 +1,8 @@
 using Algorithms.Math.Easy;
 using Algorithms.Math.Medium;
+using Math.Easy;
 
 Console.WriteLine("=== Math Algorithms ===");
 MathEasyRunner.Run();
 MathMediumRunner.Run();
+AddBinary.Run("11", "1");
