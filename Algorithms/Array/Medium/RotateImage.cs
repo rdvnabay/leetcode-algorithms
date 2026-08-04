4ac54df1f58d1aed8a61f16120404620c121307a
rdvnabay <ridvanabay@gmail.com>
2026-08-04 13:02:19 +0300

    48 - Rotate Image

---
+++ Algorithms/Array/Medium/RotateImage.cs
@@ -0,0 +1,34 @@
+﻿namespace Array.Medium;
+
+public static class RotateImage
+{
+    public static void Run(int[][] matrix)
+    {
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        int left = 0;
+        int right = rows - 1;
+
+        for (int i = 0; i < rows - 1; i++)
+        {
+            for (int j = i + 1; j < cols; j++)
+            {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+            }
+        }
+
+        while (left < right)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+}
