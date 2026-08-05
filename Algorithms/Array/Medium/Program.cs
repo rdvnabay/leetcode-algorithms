e3f6678aed959b03e2470ef954ade5d3fd8368f0
rdvnabay <ridvanabay@gmail.com>
2026-08-05 20:48:46 +0300

    73. Set Matrix Zeroes

--- Algorithms/Array/Medium/Program.cs
+++ Algorithms/Array/Medium/Program.cs
@@ -1,4 +1,5 @@
 
+using Array.Easy;
 using Array.Medium;
 
 namespace Algorithms.Array.Medium;
@@ -11,5 +12,6 @@ public static class ArrayMediumRunner
         NextPermutation.Run(new int[] { 3, 2, 1 });
         RotateImage.Run(new int[][] { [5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16] });
         SortColors.Run(new int[] { 2, 0, 2, 1, 1, 0 });
+        SetMatrixZeroes.Run(new int[][] { [1, 1, 1], [1, 0, 0], [1, 1, 1] });
     }
 }
---
+++ Algorithms/Array/Medium/SetMatrixZeroes.cs
@@ -0,0 +1,40 @@
+﻿namespace Array.Medium;
+
+public static class SetMatrixZeroes
+{
+    public static void Run(int[][] matrix)
+    {
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+
+        HashSet<int> zerosRow = new();
+        HashSet<int> zerosCol = new();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    if (!zerosRow.Contains(i))
+                        zerosRow.Add(i);
+
+                    if (!zerosCol.Contains(j))
+                        zerosCol.Add(j);
+                }
+            }
+        }
+
+        foreach (var row in zerosRow)
+        {
+            for (int j = 0; j < cols; j++)
+                matrix[row][j] = 0;
+        }
+
+        foreach (var col in zerosCol)
+        {
+            for (int j = 0; j < rows; j++)
+                matrix[j][col] = 0;
+        }
+    }
+}
