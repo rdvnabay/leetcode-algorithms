9197047bfc0a0bbce729bf1e9d5d3313e2c1b0dd
rdvnabay <ridvanabay@gmail.com>
2026-08-17 10:40:32 +0300

    1920. Build Array from Permutation

--- Algorithms/Array/Program.cs
+++ Algorithms/Array/Program.cs
@@ -48,6 +48,7 @@ UniqueNumberOfOccurrences.Run(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 });
 ValidMountainArray.Run(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 WordBreak.Run("catsandog", new List<string>() { "cats", "dog", "sand", "and", "cat" });
 ConcatenateArrayWithReverse.Run(new int[] { 1, 2, 3 });
+BuildArrayFromPermutation.Run(new int[] { 0, 2, 1, 5, 3, 4 });
 
 
 // --- Medium ------------------------------------------------------------
