c4ad9f3799b828c6aab480ae3f313fd90e1f73e3
rdvnabay <ridvanabay@gmail.com>
2026-08-17 10:52:07 +0300

    1929. Concatenation of Array

---
+++ Algorithms/Array/Easy/ConcatenationOfArray.cs
@@ -0,0 +1,17 @@
+﻿namespace Array.Easy;
+
+public static class ConcatenationOfArray
+{
+    public static int[] Run(int[] nums)
+    {
+        int[] result = new int[nums.Length * 2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[i];
+            result[nums.Length + i] = nums[i];
+        }
+
+        return result;
+    }
+}
