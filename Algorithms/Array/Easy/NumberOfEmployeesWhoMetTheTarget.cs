e2890bc56cf636f01a359ebd654c73db63e6b61c
rdvnabay <ridvanabay@gmail.com>
2026-08-17 19:44:48 +0300

    2798. Number of Employees Who Met the Target

---
+++ Algorithms/Array/Easy/NumberOfEmployeesWhoMetTheTarget.cs
@@ -0,0 +1,17 @@
+﻿namespace Array.Easy;
+
+public static class NumberOfEmployeesWhoMetTheTarget
+{
+    public static int Run(int[] hours, int target)
+    {
+        int result = 0;
+
+        foreach (var hour in hours)
+        {
+            if (hour >= target)
+                result++;
+        }
+
+        return result;
+    }
+}
