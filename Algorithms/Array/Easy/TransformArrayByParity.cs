3fea7d6a31b58be748b72c46a8e49ca662b4efe8
rdvnabay <ridvanabay@gmail.com>
2026-08-17 11:33:22 +0300

    3467. Transform Array by Parity

---
+++ Algorithms/Array/Easy/TransformArrayByParity.cs
@@ -0,0 +1,27 @@
+﻿namespace Array.Easy;
+
+public static class TransformArrayByParity
+{
+    public static int[] Run(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        int[] result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                result[left] = 0;
+                left++;
+            }
+            else
+            {
+                result[right] = 1;
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
--- Algorithms/Array/Program.cs
+++ Algorithms/Array/Program.cs
@@ -51,7 +51,7 @@ ConcatenateArrayWithReverse.Run(new int[] { 1, 2, 3 });
 BuildArrayFromPermutation.Run(new int[] { 0, 2, 1, 5, 3, 4 });
 ComputeAlternatingSum.Run(new int[] { 1, 3, 5, 7 });
 ConcatenationOfArray.Run(new int[] { 1, 3, 2, 1 });
-
+TransformArrayByParity.Run(new int[] { 4, 3, 2, 1 });
 
 // --- Medium ------------------------------------------------------------
 _3Sum.Run(new int[] { -1, 0, 1, 2, -1, -4 });
