bd8fff62b3119fd7da8cce728d17b45b9b7b11f8
rdvnabay <ridvanabay@gmail.com>
2026-08-18 21:29:02 +0300

    1464. Maximum Product of Two Elements in an Array

---
+++ Algorithms/Array/Easy/MaximumProductOfTwoElementsInAnArray.cs
@@ -0,0 +1,21 @@
+﻿namespace Array.Easy;
+
+public static class MaximumProductOfTwoElementsInAnArray
+{
+    public static int Run(int[] nums)
+    {
+        int maxValue = int.MinValue;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int value = (nums[i] - 1) * (nums[j] - 1);
+                if (value > maxValue)
+                    maxValue = value;
+            }
+        }
+
+        return maxValue;
+    }
+}
