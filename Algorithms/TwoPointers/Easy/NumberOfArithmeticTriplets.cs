f426406aa4668436fcbd630f74789b98d2b75890
rdvnabay <ridvanabay@gmail.com>
2026-08-20 20:12:08 +0300

    2367. Number of Arithmetic Triplets

---
+++ Algorithms/TwoPointers/Easy/NumberOfArithmeticTriplets.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoPointers.Easy;
+
+public static class NumberOfArithmeticTriplets
+{
+    public static int Run(int[] nums, int diff)
+    {
+        int counter = 0;
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            for (int j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+                    if ((nums[k] - nums[j] == diff) && (nums[j] - nums[i] == diff))
+                        counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
--- Algorithms/TwoPointers/Program.cs
+++ Algorithms/TwoPointers/Program.cs
@@ -7,5 +7,6 @@ Console.WriteLine("=== TwoPointers Algorithms ===");
 // --- Easy ------------------------------------------------------------
 IsSubsequence.Run("", "");
 ReversePrefixOfWord.Run("abcdefd", 'd');
+NumberOfArithmeticTriplets.Run(new int[] { 0, 1, 4, 6, 7, 10 }, 3);
 
 
