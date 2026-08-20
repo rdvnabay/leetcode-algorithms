308f22fcfae9bba606526081f145931367961b3d
rdvnabay <ridvanabay@gmail.com>
2026-08-20 20:00:30 +0300

    2000. Reverse Prefix of Word

---
+++ Algorithms/TwoPointers/Easy/ReversePrefixOfWord.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoPointers.Easy;
+
+public static class ReversePrefixOfWord
+{
+    public static string Run(string word, char ch)
+    {
+        int left = 0;
+        int right = word.IndexOf(ch);
+        var otherParts = word.Substring(right + 1, word.Length - right - 1);
+        char[] chars = new char[right - left + 1];
+
+        while (left <= right)
+        {
+            char temp = word[left];
+            chars[left] = word[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new string(chars) + otherParts;
+    }
+}
--- Algorithms/TwoPointers/Program.cs
+++ Algorithms/TwoPointers/Program.cs
@@ -1,8 +1,11 @@
 // Demo runner for 1 indexed solution files. Canonical ordering: LeetCode problem ID.
 using Algorithms.TwoPointers.Easy;
+using TwoPointers.Easy;
 
 Console.WriteLine("=== TwoPointers Algorithms ===");
 
 // --- Easy ------------------------------------------------------------
+IsSubsequence.Run("", "");
+ReversePrefixOfWord.Run("abcdefd", 'd');
 
 
