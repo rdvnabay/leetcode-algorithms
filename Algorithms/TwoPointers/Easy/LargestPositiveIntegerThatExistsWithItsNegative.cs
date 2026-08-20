1df1b514d32a39aca22beee83aa4ca58556cbc0f
rdvnabay <ridvanabay@gmail.com>
2026-08-20 21:41:42 +0300

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ Algorithms/TwoPointers/Easy/LargestPositiveIntegerThatExistsWithItsNegative.cs
@@ -0,0 +1,21 @@
+﻿namespace TwoPointers.Easy;
+
+public static class LargestPositiveIntegerThatExistsWithItsNegative
+{
+    public static int Run(int[] nums)
+    {
+        HashSet<int> set = new(nums);
+        HashSet<int> result = new();
+
+        foreach (var num in set)
+        {
+            if (set.Contains(num * -1))
+                result.Add(Math.Abs(num));
+        }
+
+        if (result.Count == 0)
+            return -1;
+
+        return result.Max();
+    }
+}
--- Algorithms/TwoPointers/Program.cs
+++ Algorithms/TwoPointers/Program.cs
@@ -11,5 +11,6 @@ NumberOfArithmeticTriplets.Run(new int[] { 0, 1, 4, 6, 7, 10 }, 3);
 LimitOccurrencesInSortedArray.Run(new int[] { 1, 1, 1, 2, 2, 3 }, 2);
 ApplyOperationsToAnArray.Run(new int[] { 1, 2, 2, 1, 1, 0 });
 FindTheArrayConcatenationValue.Run(new int[] { 5, 14, 13, 8, 12 });
+LargestPositiveIntegerThatExistsWithItsNegative.Run(new int[] { -10, 8, 6, 7, -2, -3 });
 
 
