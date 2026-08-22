0dacb013a591705818f0ba28ed65020342b82857
rdvnabay <ridvanabay@gmail.com>
2026-08-22 22:12:02 +0300

    3663. Find The Least Frequent Digit

---
+++ Algorithms/HashTable/Easy/FindTheLeastFrequentDigit.cs
@@ -0,0 +1,19 @@
+﻿namespace HashTable.Easy;
+
+public static class FindTheLeastFrequentDigit
+{
+    public static int Run(int n)
+    {
+        Dictionary<int, int> dict = new();
+
+        while (n > 0)
+        {
+            int remainder = n % 10;
+            dict[remainder] = dict.GetValueOrDefault(remainder, 0) + 1;
+
+            n /= 10;
+        }
+
+        return dict.OrderBy(x => x.Value).ThenBy(x => x.Key).First().Key;
+    }
+}
--- Algorithms/HashTable/Program.cs
+++ Algorithms/HashTable/Program.cs
@@ -51,6 +51,7 @@ ToggleLightBulbs.Run(new List<int>() { 10, 30, 20, 10 });
 MinimumNumberOfOperationsToMakeElementsInArrayDistinct.Run(new int[] { 1, 2, 3, 4, 2, 3, 3, 5, 7 });
 FindResultantArrayAfterRemovingAnagrams.Run(new string[] { "abba", "baba", "bbaa", "cd", "cd" });
 IntersectionOfMultipleArrays.Run(new int[][] { [3, 1, 2, 4, 5], [1, 2, 3, 4], [3, 4, 5, 6] });
+FindTheLeastFrequentDigit.Run(723344511);
 
 
 // --- Hard ------------------------------------------------------------
