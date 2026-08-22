5b1004b730d5d5b7269a08f4f2dd3e1e4c314b5d
rdvnabay <ridvanabay@gmail.com>
2026-08-22 18:01:47 +0300

    2154. Keep Multiplying Found Values by Two

---
+++ Algorithms/HashTable/Easy/KeepMultiplyingFoundValuesByTwo.cs
@@ -0,0 +1,14 @@
+﻿namespace HashTable.Easy;
+
+public static class KeepMultiplyingFoundValuesByTwo
+{
+    public static int Run(int[] nums, int original)
+    {
+        HashSet<int> set = new(nums);
+
+        while (set.Contains(original))
+            original *= 2;
+
+        return original;
+    }
+}
--- Algorithms/HashTable/Program.cs
+++ Algorithms/HashTable/Program.cs
@@ -34,6 +34,7 @@ TwoOutOfThree.Run(new int[] { 1, 1, 3, 2 }, new int[] { 2, 3 }, new int[] { 3 })
 MaximumNumberOfPairsInArray.Run(new int[] { 1, 3, 2, 1, 3, 2, 2 });
 FindLuckyIntegerInAnArray.Run(new int[] { 1, 2, 2, 3, 3, 3 });
 FirstLetterToAppearTwice.Run("eesll");
+KeepMultiplyingFoundValuesByTwo.Run(new int[] { 5, 3, 6, 1, 12 }, 3);
 
 
 // --- Hard ------------------------------------------------------------
