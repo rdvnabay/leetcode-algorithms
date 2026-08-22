64504541f6024e07a5605793809eeeb2acf6f294
rdvnabay <ridvanabay@gmail.com>
2026-08-22 17:47:26 +0300

    1394. Find Lucky Integer in an Array

---
+++ Algorithms/HashTable/Easy/FindLuckyIntegerInAnArray.cs
@@ -0,0 +1,26 @@
+﻿namespace HashTable.Easy;
+
+public static class FindLuckyIntegerInAnArray
+{
+    //1,2,2,3,3,3
+    public static int Run(int[] arr)
+    {
+        int result = -1;
+        Dictionary<int, int> dict = new();
+
+        foreach (var item in arr)
+            dict[item] = dict.GetValueOrDefault(item, 0) + 1;
+
+
+        foreach (var item in dict)
+        {
+            if (item.Key == item.Value)
+            {
+                if (item.Key > result)
+                    result = item.Key;
+            }
+        }
+
+        return result;
+    }
+}
--- Algorithms/HashTable/Program.cs
+++ Algorithms/HashTable/Program.cs
@@ -32,6 +32,7 @@ Finding3DigitEvenNumbers.Run(new int[] { 2, 2, 8, 8, 2 });
 SumOfElementsWithFrequencyDivisibleByK.Run(new int[] { 1, 2, 2, 3, 3, 3, 3, 4 }, 2);
 TwoOutOfThree.Run(new int[] { 1, 1, 3, 2 }, new int[] { 2, 3 }, new int[] { 3 });
 MaximumNumberOfPairsInArray.Run(new int[] { 1, 3, 2, 1, 3, 2, 2 });
+FindLuckyIntegerInAnArray.Run(new int[] { 1, 2, 2, 3, 3, 3 });
 
 
 // --- Hard ------------------------------------------------------------
