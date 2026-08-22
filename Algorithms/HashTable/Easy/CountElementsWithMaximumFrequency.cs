8f197f8d2dd1b53564cc5a84195ed6c39e1bc655
rdvnabay <ridvanabay@gmail.com>
2026-08-22 14:13:09 +0300

    3005. Count Elements With Maximum Frequency

---
+++ Algorithms/HashTable/Easy/CountElementsWithMaximumFrequency.cs
@@ -0,0 +1,37 @@
+﻿namespace HashTable.Easy;
+
+public static class CountElementsWithMaximumFrequency
+{
+    public static int Run(int[] nums)
+    {
+        Dictionary<int, int> dict = new();
+        int max = 0;
+
+        foreach (var num in nums)
+        {
+            if (!dict.TryGetValue(num, out int count))
+            {
+                count = 1;
+                dict[num] = count;
+            }
+            else
+            {
+                count = count + 1;
+                dict[num] = count;
+            }
+
+            if (count > max)
+                max = count;
+        }
+
+        int total = 0;
+        foreach (var item in dict)
+        {
+            if (item.Value == max)
+                total += item.Value;
+        }
+
+
+        return total;
+    }
+}
--- Algorithms/HashTable/Program.cs
+++ Algorithms/HashTable/Program.cs
@@ -25,6 +25,7 @@ MaximumNumberOfWordsYouCanType.Run("leet code", "lt");
 MergeTwo2DArraysBySummingValues.Run(new int[][] { [2, 4], [3, 6], [5, 5] }, new int[][] { [1, 3], [4, 3] });
 FindTheDifferenceOfTwoArrays.Run(new int[] { 1, 2, 3 }, new int[] { 2, 4, 6 });
 RingsAndRods.Run("B0B6G0R6R0R6G9");
+CountElementsWithMaximumFrequency.Run(new int[] { 1, 2, 3, 4, 5 });
 
 
 // --- Hard ------------------------------------------------------------
