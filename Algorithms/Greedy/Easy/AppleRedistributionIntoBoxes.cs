23a72fa616607f25029e919fa0ed2c77cd06c197
rdvnabay <ridvanabay@gmail.com>
2026-08-23 12:21:14 +0300

    3074. Apple Redistribution into Boxes

---
+++ Algorithms/Greedy/Easy/AppleRedistributionIntoBoxes.cs
@@ -0,0 +1,26 @@
+﻿namespace Greedy.Easy;
+
+public static class AppleRedistributionIntoBoxes
+{
+    //Input: apple = [1, 3, 2], capacity = [4, 3, 1, 5, 2]
+    //Output: 2
+    public static int Run(int[] apple, int[] capacity)
+    {
+        int totalCapacity = 0;
+        int totalAppleCount = 0;
+
+        for (int i = 0; i < apple.Length; i++)
+            totalAppleCount += apple[i];
+
+        Array.Sort(capacity, (a, b) => b.CompareTo(a));
+
+        int index = 0;
+        while (totalCapacity < totalAppleCount)
+        {
+            totalCapacity += capacity[index];
+            index++;
+        }
+
+        return index;
+    }
+}
--- Algorithms/Greedy/Program.cs
+++ Algorithms/Greedy/Program.cs
@@ -11,5 +11,6 @@ TeemoAttacking.Run(new int[] { 1, 3, 8 }, 4);
 MinimumOperationsToMakeTheArrayIncreasing.Run(new int[] { 1, 5, 2, 4, 1 });
 LexicographicallySmallestPalindrome.Run("seven");
 MaximumSumWithExactlyKElements.Run(new int[] { 5, 5 }, 2);
+AppleRedistributionIntoBoxes.Run(new int[] { 1, 3, 2 }, new int[] { 4, 3, 1, 5, 2 });
 
 
