1a9b3f98517722df1e8c0ce04b89fd41d9c73363
rdvnabay <ridvanabay@gmail.com>
2026-08-23 11:33:11 +0300

    1827. Minimum Operations to Make the Array Increasing

---
+++ Algorithms/Greedy/Easy/MinimumOperationsToMakeTheArrayIncreasing.cs
@@ -0,0 +1,21 @@
+﻿namespace Greedy.Easy;
+
+public static class MinimumOperationsToMakeTheArrayIncreasing
+{
+    public static int Run(int[] nums)
+    {
+        int total = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] >= nums[i + 1])
+            {
+                int increaceCount = (nums[i] - nums[i + 1]) + 1;
+                nums[i + 1] += increaceCount;
+                total += increaceCount;
+            }
+        }
+
+        return total;
+    }
+}
--- Algorithms/Greedy/Program.cs
+++ Algorithms/Greedy/Program.cs
@@ -1,5 +1,6 @@
 // Demo runner for 3 indexed solution files. Canonical ordering: LeetCode problem ID.
 using Algorithms.Greedy.Easy;
+using Greedy.Easy;
 
 Console.WriteLine("=== Greedy Algorithms ===");
 
@@ -7,5 +8,6 @@ Console.WriteLine("=== Greedy Algorithms ===");
 AssignCookies.Run(new int[] { 1, 2, 3 }, new int[] { 1, 1 });
 JumpGame.Run(new int[] { 3, 2, 1, 0, 4 });
 TeemoAttacking.Run(new int[] { 1, 3, 8 }, 4);
+MinimumOperationsToMakeTheArrayIncreasing.Run(new int[] { 1, 5, 2, 4, 1 });
 
 
