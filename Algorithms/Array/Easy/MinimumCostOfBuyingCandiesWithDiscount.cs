81b2912d909d8be5db7d9b240c5822c9ec05f484
rdvnabay <ridvanabay@gmail.com>
2026-08-24 14:10:23 +0300

    2144. Minimum Cost of Buying Candies With Discount

---
+++ Algorithms/Array/Easy/MinimumCostOfBuyingCandiesWithDiscount.cs
@@ -0,0 +1,25 @@
+﻿namespace Array.Easy;
+
+public static class MinimumCostOfBuyingCandiesWithDiscount
+{
+    public static int Run(int[] cost)
+    {
+        System.Array.Sort(cost, (a, b) => b.CompareTo(a));
+        int totalAmount = 0;
+
+        for (int i = 0; i < cost.Length; i++)
+        {
+            if ((i + 1) % 3 == 0)
+            {
+                if (cost[i] > cost[i - 1])
+                    totalAmount += cost[i];
+            }
+            else
+            {
+                totalAmount += cost[i];
+            }
+        }
+
+        return totalAmount;
+    }
+}
