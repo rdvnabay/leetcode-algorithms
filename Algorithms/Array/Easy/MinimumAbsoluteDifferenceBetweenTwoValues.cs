aaaa813bb9f6f95e1d729d76db47faa6a66028fc
rdvnabay <ridvanabay@gmail.com>
2026-08-24 17:01:04 +0300

    3880. Minimum Absolute Difference Between Two Values

---
+++ Algorithms/Array/Easy/MinimumAbsoluteDifferenceBetweenTwoValues.cs
@@ -0,0 +1,35 @@
+﻿namespace Array.Easy;
+
+public static class MinimumAbsoluteDifferenceBetweenTwoValues
+{
+    public static int Run(int[] nums)
+    {
+        List<int> numberOneIndexes = new();
+        List<int> numberTwoIndexes = new();
+        int minResult = int.MaxValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+                numberOneIndexes.Add(i);
+
+            else if (nums[i] == 2)
+                numberTwoIndexes.Add(i);
+        }
+
+        if (numberOneIndexes.Count < 1 || numberTwoIndexes.Count < 1)
+            return -1;
+
+        for (int i = 0; i < numberOneIndexes.Count; i++)
+        {
+            for (int j = 0; j < numberTwoIndexes.Count; j++)
+            {
+                int diff = Math.Abs(numberOneIndexes[i] - numberTwoIndexes[j]);
+                if (diff < minResult)
+                    minResult = diff;
+            }
+        }
+
+        return minResult;
+    }
+}
--- Algorithms/Array/Program.cs
+++ Algorithms/Array/Program.cs
@@ -81,6 +81,7 @@ MinimumCostOfBuyingCandiesWithDiscount.Run(new int[] { 6, 5, 7, 9, 2, 2 });
 Unique3DigitEvenNumbers.Run(new int[] { 1, 2, 3, 4 });
 LastStoneWeight.Run(new int[] { 2, 7, 4, 1, 8, 1 });
 CountDominantIndices.Run(new int[] { 5, 4, 3 });
+MinimumAbsoluteDifferenceBetweenTwoValues.Run(new int[] { 1, 0, 1, 0 });
 
 
 // --- Medium ------------------------------------------------------------
