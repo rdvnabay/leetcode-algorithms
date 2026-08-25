b3ed559227c2c53e96b7e0e56449d8606a2785c2
rdvnabay <ridvanabay@gmail.com>
2026-08-25 18:59:22 +0300

    2124. Check if All A's Appears Before All B's

---
+++ Algorithms/String/Easy/CheckIfAllAsAppearsBeforeAllBs.cs
@@ -0,0 +1,15 @@
+﻿namespace String.Easy;
+
+public static class CheckIfAllAsAppearsBeforeAllBs
+{
+    public static bool Run(string s)
+    {
+        for (int i = 0; i < s.Length-1; i++)
+        {
+            if (s[i] == 'b' && s[i + 1] == 'a')
+                return false;
+        }
+
+        return true;
+    }
+}
