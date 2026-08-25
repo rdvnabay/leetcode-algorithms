5e64448fcd6894e362539929ef9e0ee98228ac5c
rdvnabay <ridvanabay@gmail.com>
2026-08-25 18:24:20 +0300

    2278. Percentage of Letter in String

---
+++ Algorithms/String/Easy/PercentageOfLetterInString.cs
@@ -0,0 +1,17 @@
+﻿namespace String.Easy;
+
+public static class PercentageOfLetterInString
+{
+    public static int Run(string s, char letter)
+    {
+        int matchLetterCount = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == letter)
+                matchLetterCount++;
+        }
+
+        return matchLetterCount * 100 / s.Length;
+    }
+}
