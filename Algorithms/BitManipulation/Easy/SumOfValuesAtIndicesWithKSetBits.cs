5c3454da202cd872146a6b48edfccd483cceea35
rdvnabay <ridvanabay@gmail.com>
2026-08-28 18:29:44 +0300

    2859. Sum of Values at Indices With K Set Bits

---
+++ Algorithms/BitManipulation/Easy/SumOfValuesAtIndicesWithKSetBits.cs
@@ -0,0 +1,29 @@
+﻿namespace BitManipulation.Easy;
+
+public static class SumOfValuesAtIndicesWithKSetBits
+{
+    public static int Run(IList<int> nums, int k)
+    {
+        int result = 0;
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            int index = i;
+            int onesTotal = 0;
+
+            while (index > 0)
+            {
+                int remainder = index % 2;
+                if (remainder == 1)
+                    onesTotal++;
+
+                index /= 2;
+            }
+
+            if (onesTotal == k)
+                result += nums[i];
+        }
+
+        return result;
+    }
+}
--- Algorithms/BitManipulation/Program.cs
+++ Algorithms/BitManipulation/Program.cs
@@ -7,5 +7,5 @@ Console.WriteLine("=== BitManipulation Algorithms ===");
 // --- Easy ------------------------------------------------------------
 NumberOf1Bits.Run(1);
 MinimumBitFlipsToConvertNumber.Run(10, 7);
-
+SumOfValuesAtIndicesWithKSetBits.Run(new List<int>() { 5, 10, 1, 5, 2 }, 1);
 
