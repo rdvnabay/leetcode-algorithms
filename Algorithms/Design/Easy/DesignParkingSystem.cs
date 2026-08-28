d8c4a4a257b2f48b01e243acd2cb1fafb582cb48
rdvnabay <ridvanabay@gmail.com>
2026-08-28 12:37:21 +0300

    1603. Design Parking System

---
+++ Algorithms/Design/Easy/DesignParkingSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace Design.Easy;
+
+public class DesignParkingSystem
+{
+    private readonly int[] _spaces;
+    public DesignParkingSystem(int big, int medium, int small)
+    {
+        _spaces = [big, medium, small];
+    }
+
+    public bool AddCar(int carType)
+    {
+        if (_spaces[carType - 1] == 0)
+            return false;
+
+        _spaces[carType - 1]--;
+        return true;
+    }
+}
---
+++ Algorithms/Design/Program.cs
@@ -0,0 +1,7 @@
+﻿using Design.Easy;
+
+DesignParkingSystem parkingSystem = new(1, 1, 0);
+parkingSystem.AddCar(1);
+parkingSystem.AddCar(2);
+parkingSystem.AddCar(3);
+parkingSystem.AddCar(1);
