368585416b6774674bd281a5f7b92e3dff1ebf88
rdvnabay <ridvanabay@gmail.com>
2026-08-28 15:32:01 +0300

    1791. Find Center of Star Graph

---
+++ Algorithms/GraphTheory/Easy/FindCenterOfStarGraph.cs
@@ -0,0 +1,14 @@
+﻿namespace GraphTheory.Easy;
+
+public static class FindCenterOfStarGraph
+{
+    public static int Run(int[][] edges)
+    {
+        int left = edges[0][0];
+        int right = edges[0][1];
+
+        return edges[1][0] == left || edges[1][1] == left
+            ? left
+            : right;
+    }
+}
---
+++ Algorithms/GraphTheory/Program.cs
@@ -0,0 +1,3 @@
+﻿using GraphTheory.Easy;
+
+FindCenterOfStarGraph.Run(new int[][] { [1, 2], [5, 1], [1, 3], [1, 4] });
