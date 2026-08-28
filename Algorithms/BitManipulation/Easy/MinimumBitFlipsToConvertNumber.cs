653d5f02bbc5d719fa4ee674530df2c578e17268
rdvnabay <ridvanabay@gmail.com>
2026-08-28 12:16:35 +0300

    2220. Minimum Bit Flips to Convert Number

---
+++ Algorithms/BitManipulation/Easy/MinimumBitFlipsToConvertNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace BitManipulation.Easy;
+
+public static class MinimumBitFlipsToConvertNumber
+{
+    public static int Run(int start, int goal)
+    {
+        int maxNumber = Math.Max(start, goal);
+        int length = Convert.ToString(maxNumber, 2).Length;
+
+        string startBinary = Convert.ToString(start, 2).PadLeft(length, '0');
+        string goalBinary = Convert.ToString(goal, 2).PadLeft(length, '0');
+      
+        int counter = 0;
+ 
+        for (int i = 0; i < length; i++)
+        {
+            if (startBinary[i] != goalBinary[i])
+                counter++;
+        }
+
+        return counter;
+    }
+}
--- Algorithms/BitManipulation/Program.cs
+++ Algorithms/BitManipulation/Program.cs
@@ -1,9 +1,11 @@
 // Demo runner for 1 solution files; the README is the canonical ID-sorted index.
 using Algorithms.BitManipulation.Easy;
+using BitManipulation.Easy;
 
 Console.WriteLine("=== BitManipulation Algorithms ===");
 
 // --- Easy ------------------------------------------------------------
 NumberOf1Bits.Run(1);
+MinimumBitFlipsToConvertNumber.Run(10, 7);
 
 
