4aea9c7914a78eb07254af82fef1f0126512fa03
rdvnabay <ridvanabay@gmail.com>
2026-08-29 10:44:33 +0300

    1380. Lucky Numbers in a Matrix

---
+++ Algorithms/Matrix/Easy/LuckyNumbersInAMatrix.cs
@@ -0,0 +1,42 @@
+﻿namespace Matrix.Easy;
+
+public static class LuckyNumbersInAMatrix
+{
+    public static IList<int> Run(int[][] matrix)
+    {
+        int rows = matrix.Length; 
+        int cols = matrix[0].Length;
+        List<int> result = new();
+
+        for (int i = 0; i < rows; i++)
+        {
+            int minRowValue = int.MaxValue;
+            int findColumnIndex = 0;
+
+            for (int j = 0; j < cols; j++)
+            {
+                int value = matrix[i][j];
+                if (value < minRowValue)
+                {
+                    minRowValue = value;
+                    findColumnIndex = j;
+                }
+            }
+
+            bool isColumnMaximum = true;
+            for (int k = 0; k < rows; k++)
+            {
+                if (minRowValue < matrix[k][findColumnIndex])
+                {
+                    isColumnMaximum = false;
+                    break;
+                }
+            }
+
+            if (isColumnMaximum)
+                result.Add(minRowValue);
+        }
+
+        return result;
+    }
+}
---
+++ Algorithms/Matrix/Program.cs
@@ -0,0 +1,3 @@
+﻿using Matrix.Easy;
+
+LuckyNumbersInAMatrix.Run(new int[][] { [1, 10, 4, 2], [9, 3, 8, 7], [15, 16, 17, 12] });
