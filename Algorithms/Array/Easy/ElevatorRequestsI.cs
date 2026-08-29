65201ad0f96d34ff9b005f1d2211213df5c8d27d
rdvnabay <ridvanabay@gmail.com>
2026-08-29 09:48:07 +0300

    4020. Elevator Requests I

---
+++ Algorithms/Array/Easy/ElevatorRequestsI.cs
@@ -0,0 +1,14 @@
+﻿namespace Array.Easy;
+
+public static class ElevatorRequestsI
+{
+    public static int Run(int n, int[] requests)
+    {
+        int total = requests[0];
+
+        for (int i = 0; i < requests.Length - 1; i++)
+            total += Math.Abs(requests[i] - requests[i + 1]);
+        
+        return total;
+    }
+}
--- Algorithms/Array/Program.cs
+++ Algorithms/Array/Program.cs
@@ -96,6 +96,7 @@ LargestLocalValuesInAMatrix.Run(new int[][] { [9, 9, 8, 1], [5, 6, 2, 6], [8, 2,
 DistributeElementsIntoTwoArraysI.Run(new int[] { 5, 4, 3, 8 });
 SumOfSquaresOfSpecialElements.Run(new int[] { 2, 7, 1, 19, 18, 3 });
 CountIndicesWithOppositeParity.Run(new int[] { 1, 2, 3, 4 });
+ElevatorRequestsI.Run(5, [2, 1, 4, 3]);
 
 
 // --- Medium ------------------------------------------------------------
