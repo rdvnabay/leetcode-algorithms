92ba0bcf5c6e6703d9eea4b9d44689046fb3afd9
rdvnabay <ridvanabay@gmail.com>
2026-08-29 12:02:20 +0300

    3884. First Matching Character From Both Ends

---
+++ Algorithms/TwoPointers/Easy/FirstMatchingCharacterFromBothEnds.cs
@@ -0,0 +1,15 @@
+﻿namespace TwoPointers.Easy;
+
+public static class FirstMatchingCharacterFromBothEnds
+{
+    public static int Run(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == s[s.Length - i - 1])
+                return i;
+        }
+
+        return -1;
+    }
+}
