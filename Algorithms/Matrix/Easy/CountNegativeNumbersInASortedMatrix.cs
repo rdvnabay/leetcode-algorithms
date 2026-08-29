f8420d777a0f5b1ab40384f6a9615608c6d38c22
rdvnabay <ridvanabay@gmail.com>
2026-08-29 11:36:26 +0300

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ Algorithms/Matrix/Easy/CountNegativeNumbersInASortedMatrix.cs
@@ -0,0 +1,25 @@
+﻿namespace Matrix.Easy;
+
+public static class CountNegativeNumbersInASortedMatrix
+{
+    public static int Run(int[][] grid)
+    {
+        int negativeCount = 0;
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] < 0)
+                {
+                    negativeCount += cols - j;
+                    break;
+                }
+            }
+        }
+
+        return negativeCount;
+    }
+}
