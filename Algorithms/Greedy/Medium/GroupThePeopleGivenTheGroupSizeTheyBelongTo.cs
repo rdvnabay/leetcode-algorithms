a9ccca7f1e6a5f364a7d8d2ecfa220f6b9d4ffec
rdvnabay <ridvanabay@gmail.com>
2026-08-31 10:53:44 +0300

    1282. Group the People Given the Group Size They Belong To

---
+++ Algorithms/Greedy/Medium/GroupThePeopleGivenTheGroupSizeTheyBelongTo.cs
@@ -0,0 +1,36 @@
+﻿namespace Greedy.Medium;
+
+public static class GroupThePeopleGivenTheGroupSizeTheyBelongTo
+{
+    public static IList<IList<int>> Run(int[] groupSizes)
+    {
+        Dictionary<int, List<int>> dict = new();
+        List<IList<int>> result = new();
+
+        for (int i = 0; i < groupSizes.Length; i++)
+        {
+            int size = groupSizes[i];
+            if (!dict.TryGetValue(size, out _))
+                dict[size] = new() { i };
+            else
+                dict[size].Add(i);
+        }
+
+        foreach (var item in dict)
+        {
+            int count = item.Value.Count;
+            int start = 0;
+
+            while (count / item.Key > 0)
+            {
+                var values = item.Value.Skip(start).Take(item.Key).ToList();
+                result.Add(values);
+
+                count -= item.Key;
+                start += item.Key;
+            }
+        }
+
+        return result;
+    }
+}
--- Algorithms/Greedy/Program.cs
+++ Algorithms/Greedy/Program.cs
@@ -1,6 +1,7 @@
 // Demo runner for 16 solution files; the README is the canonical ID-sorted index.
 using Algorithms.Greedy.Easy;
 using Greedy.Easy;
+using Greedy.Medium;
 
 Console.WriteLine("=== Greedy Algorithms ===");
 
@@ -13,7 +14,7 @@ LexicographicallySmallestPalindrome.Run("seven");
 MaximumSumWithExactlyKElements.Run(new int[] { 5, 5 }, 2);
 AppleRedistributionIntoBoxes.Run(new int[] { 1, 3, 2 }, new int[] { 4, 3, 1, 5, 2 });
 MaximizeSumOfAtMostKDistinctElements.Run(new int[] { 84, 93, 100, 77, 90 }, 3);
-MaximumDifferenceByRemappingADigit.Run2(90);
+MaximumDifferenceByRemappingADigit.Run(11891);
 MaximumUnitsOnATruck.Run(new int[][] { [1, 3], [2, 2], [3, 1] }, 4);
 MinimumSubsequenceInNonIncreasingOrder.Run(new int[] { 4, 3, 10, 9, 8 });
 BuyTwoChocolates.Run(new int[] { 1, 2, 2 }, 3);
@@ -21,6 +22,12 @@ MinimumNumberOfOperationsToConvertTime.Run("02:30", "04:35");
 LargestIntegerWithGivenDigitSum.Run(2, 9);
 MinimumMovesToConvertString.Run("XXOX");
 MinimumAmountOfTimeToFillCups.Run(new int[] { 1, 4, 2 });
+MinimumTimeToTypeWordUsingSpecialTypewriter.Run("bza");
+LatestTimeByReplacingHiddenDigits.Run("0?:3?");
+
+
+// --- Medium ------------------------------------------------------------
+GroupThePeopleGivenTheGroupSizeTheyBelongTo.Run([3, 3, 3, 3, 3, 1, 3]);
 
 
 
