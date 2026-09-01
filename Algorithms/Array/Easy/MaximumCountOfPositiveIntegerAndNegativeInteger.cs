b78db49f0a5cddc21a0e38372bcd16ac58a8aef1
rdvnabay <ridvanabay@gmail.com>
2026-09-01 11:50:31 +0300

    2529. Maximum Count of Positive Integer and Negative Integer

---
+++ Algorithms/Array/Easy/MaximumCountOfPositiveIntegerAndNegativeInteger.cs
@@ -0,0 +1,26 @@
+﻿namespace Array.Easy;
+
+public static class MaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    public static int Run(int[] nums)
+    {
+        int zerosCount = 0;
+        int negativeCount = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+
+            if (num < 0)
+                negativeCount++;
+            else if (num == 0)
+                zerosCount++;
+            else
+                break;
+        }
+
+        int positiveCount = nums.Length - negativeCount - zerosCount;
+
+        return Math.Max(positiveCount, negativeCount);
+    }
+}
