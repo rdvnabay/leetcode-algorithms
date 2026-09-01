51de45e267c1b9d627606363e5caa3e2ddf8867f
rdvnabay <ridvanabay@gmail.com>
2026-09-01 22:28:42 +0300

    3079. Find the Sum of Encrypted Integers

---
+++ Algorithms/Array/Easy/FindTheSumOfEncryptedIntegers.cs
@@ -0,0 +1,29 @@
+﻿namespace Array.Easy;
+
+public static class FindTheSumOfEncryptedIntegers
+{
+    public static int Run(int[] nums)
+    {
+        int total = 0;
+
+        foreach (var num in nums)
+        {
+            int maxDigit = 0;
+            int digitCount = 0;
+            int currentNum = num;
+
+            while (currentNum > 0)
+            {
+                int remainder = currentNum % 10;
+                maxDigit = Math.Max(maxDigit, remainder);
+                digitCount++;
+                currentNum /= 10;
+            }
+
+            string value = string.Concat(Enumerable.Repeat(maxDigit, digitCount));
+            total += int.Parse(value);
+        }
+
+        return total;
+    }
+}
--- Algorithms/Array/Program.cs
+++ Algorithms/Array/Program.cs
@@ -104,6 +104,7 @@ FindTheDistinctDifferenceArray.Run([1, 2, 3, 4, 5]);
 AbsoluteDifferenceBetweenMaximumAndMinimumKElements.Run([5, 2, 2, 4], 2);
 MaximumDifferenceBetweenAdjacentElementsInACircularArray.Run([-5, -10, -5]);
 MinimumAbsoluteDifference.Run([4, 2, 1, 3]);
+FindTheSumOfEncryptedIntegers.Run([10, 21, 31]);
 
 
 // --- Medium ------------------------------------------------------------
