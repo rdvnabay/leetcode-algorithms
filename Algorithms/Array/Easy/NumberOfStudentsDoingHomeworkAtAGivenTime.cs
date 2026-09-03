d2329435f4993c9fa5946b7b76de2206951cde33
rdvnabay <ridvanabay@gmail.com>
2026-09-03 18:57:28 +0300

    1450. Number of Students Doing Homework at a Given Time

---
+++ Algorithms/Array/Easy/NumberOfStudentsDoingHomeworkAtAGivenTime.cs
@@ -0,0 +1,17 @@
+﻿namespace Array.Easy;
+
+public static class NumberOfStudentsDoingHomeworkAtAGivenTime
+{
+    public static int Run(int[] startTime, int[] endTime, int queryTime)
+    {
+        int counter = 0;
+
+        for (int i = 0; i < startTime.Length; i++)
+        {
+            if ((startTime[i] <= queryTime) && (endTime[i] >= queryTime))
+                counter++;
+        }
+
+        return counter;
+    }
+}
